52b720519780a52f574d2c79d933176c0c6f61bc
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-09-06 03:25:26 +0800

    Delete 495 - Fibonacci Freeze.py

--- 495 - Fibonacci Freeze.py
+++
@@ -1,9 +0,0 @@
-fibonacci=[0, 1]
-for i in range(2, 5001):
-    fibonacci.append(fibonacci[-1]+fibonacci[-2])
-try:
-    while True:
-        n=int(input())
-        print(f'The Fibonacci number for {n} is {fibonacci[n]}')
-except EOFError:
-    pass
